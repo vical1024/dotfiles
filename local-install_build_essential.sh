422ac8aa8a2a081cd4268b83ddcd025c64f1097e
Minwoo Logan Lee <vical1024@gmail.com>
2024-06-02 17:45:48 +0900

    Update local-install_build_essential.sh

--- local-install_build_essential.sh
+++ local-install_build_essential.sh
@@ -80,6 +80,11 @@ install_binutils() {
     check_status "Installed Binutils in ~/.local/share" "Failed to install Binutils in ~/.local/share"
 }
 
+install_rust() 
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+}
+
+
 echo_blue "====================================================="
 echo_blue " Install build enssential into .local without using apt
 echo_blue "====================================================="
@@ -88,3 +93,4 @@ echo_blue "====================================================="
 install_gcc
 install_make
 install_binutils
+install rust

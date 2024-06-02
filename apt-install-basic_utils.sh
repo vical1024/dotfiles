24af2ec95079420c5af9e514fd7ffd3b485f11c1
Minwoo Logan Lee <vical1024@gmail.com>
2024-06-02 19:08:53 +0900

    Update apt-install-basic_utils.sh

--- apt-install-basic_utils.sh
+++ apt-install-basic_utils.sh
@@ -22,10 +22,6 @@ echo_yellow() {
     echo -e "${YELLOW}$1${NO_COLOR}"
 }
 
-echo_blue "====================================================="
-echo_blue " Install curl git using apt"
-echo_blue "====================================================="
-
 check_status() {
     if [ $? -eq 0 ]; then
         echo_green "$1"
@@ -35,14 +31,29 @@ check_status() {
     fi
 }
 
-echo_yellow "Setting up another mirror server"
-sudo sed -i 's|http://archive.ubuntu.com/ubuntu/|http://mirror.math.princeton.edu/pub/ubuntu/|g' /etc/apt/sources.list
+setup_mirror_server() {
+    echo_yellow "Setting up another mirror server"
+    sudo sed -i 's|http://archive.ubuntu.com/ubuntu/|http://mirror.math.princeton.edu/pub/ubuntu/|g' /etc/apt/sources.list
+}
 
-echo_yellow "Clearing local cache and retrying package update"
-sudo rm -rf /var/lib/apt/lists/*
-sudo apt update
-check_status "Cleared local cache and updated package list" "Failed to clear local cache or update package list"
+clear_cache_and_update() {
+    echo_yellow "Clearing local cache and retrying package update"
+    sudo rm -rf /var/lib/apt/lists/*
+    sudo apt update
+    check_status "Cleared local cache and updated package list" "Failed to clear local cache or update package list"
+}
 
-echo_yellow "Installing necessary packages"
-sudo apt install -y curl git
-check_status "Installed necessary packages" "Failed to install necessary packages"
+install_necessary_packages() {
+    echo_yellow "Installing necessary packages"
+    sudo apt install -y curl git
+    check_status "Installed necessary packages" "Failed to install necessary packages"
+}
+
+
+echo_blue "====================================================="
+echo_blue " Install curl git using apt"
+echo_blue "====================================================="
+
+setup_mirror_server
+clear_cache_and_update
+install_necessary_packages

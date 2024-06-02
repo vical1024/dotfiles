bb3f0b4f6d42082493ca9472cc957ec8b6cf3d49
Minwoo Logan Lee <vical1024@gmail.com>
2024-06-02 15:25:42 +0900

    Update install.sh

--- nvim/install.sh
+++ nvim/install.sh
@@ -3,50 +3,67 @@
 RED='\033[1;31m'
 GREEN='\033[1;32m'
 BLUE='\033[1;34m'
+YELLOW='\033[1;33m'
 NO_COLOR='\033[0m'
 
-echo -e "${BLUE}Execute install.sh for nvim${NO_COLOR}"
+echo_blue() {
+    echo -e "${BLUE}$1${NO_COLOR}"
+}
+
+echo_green() {
+    echo -e "${GREEN}$1${NO_COLOR}"
+}
+
+echo_red() {
+    echo -e "${RED}$1${NO_COLOR}"
+}
+
+echo_yellow() {
+    echo -e "${YELLOW}$1${NO_COLOR}"
+}
+
+echo_blue "Execute install.sh for nvim"
 
 check_status() {
     if [ $? -eq 0 ]; then
-        echo -e "${GREEN}$1${NO_COLOR}"
+        echo_green "$1"
     else
-        echo -e "${RED}$2${NO_COLOR}"
+        echo_red "$2"
         exit 1
     fi
 }
 
 if [ ! -d "$HOME/.local" ]; then
-    sudo mkdir -p "$HOME/.local"
+    mkdir -p "$HOME/.local"
     check_status "Created $HOME/.local" "Failed to create $HOME/.local"
 fi
 
 if [ ! -d "$HOME/.local/bin" ]; then
-    sudo mkdir "$HOME/.local/bin"
+    mkdir "$HOME/.local/bin"
     check_status "Created $HOME/.local/bin" "Failed to create $HOME/.local/bin"
 fi
 
 if [ ! -d "$HOME/.local/share" ]; then
-    sudo mkdir -p "$HOME/.local/share"
+    mkdir -p "$HOME/.local/share"
     check_status "Created $HOME/.local/share" "Failed to create $HOME/.local/share"
 fi
 
 if [ ! -d "$HOME/.local/share/nvim" ]; then
     cd $HOME/.local/share
     check_status "Changed directory to $HOME/.local/share" "Failed to change directory to $HOME/.local/share"
-    
-    sudo mkdir -p "$HOME/.local/share/nvim"
+
+    mkdir -p "$HOME/.local/share/nvim"
     check_status "Created $HOME/.local/share/nvim" "Failed to create $HOME/.local/share/nvim"
-    
-    sudo curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+
+    curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
     check_status "Downloaded nvim tar.gz" "Failed to download nvim tar.gz"
-    
-    sudo tar -xzf nvim-linux64.tar.gz --one-top-level=nvim --strip-components=1
+
+    tar -xzf nvim-linux64.tar.gz --one-top-level=nvim --strip-components=1
     check_status "Extracted nvim tar.gz" "Failed to extract nvim tar.gz"
-    
-    sudo ln -fs $HOME/.local/share/nvim/bin/nvim $HOME/.local/bin/nvim
+
+    ln -fs $HOME/.local/share/nvim/bin/nvim $HOME/.local/bin/nvim
     check_status "Made a symbolic link for nvim to .local/bin" "Failed to make a symbolic link for nvim to .local/bin"
-    
+
     if [[ ":$PATH:" != *":$HOME/.local/bin:"* ]]; then
         echo 'export PATH="$HOME/.local/bin:$PATH"' >> ~/.profile
         check_status "Added .local/bin to PATH" "Failed to add .local/bin to PATH"
@@ -54,14 +71,43 @@ if [ ! -d "$HOME/.local/share/nvim" ]; then
 fi
 
 if [ -f "$HOME/.local/share/nvim-linux64.tar.gz" ]; then
-    sudo rm $HOME/.local/share/nvim-linux64.tar.gz
+    rm $HOME/.local/share/nvim-linux64.tar.gz
     check_status "Deleted nvim tar.gz" "Failed to delete nvim tar.gz"
 fi
 
-# Checking if nvim is properly installed
 if [ -x "$HOME/.local/bin/nvim" ]; then
-    echo -e "${GREEN}Neovim installation completed successfully.${NO_COLOR}"
+    echo_green "Neovim installation completed successfully."
+    source ~/.profile
 else
-    echo -e "${RED}Neovim installation failed.${NO_COLOR}"
+    echo_red "Neovim installation failed."
+    exit 1
+fi
+
+echo_yellow "Try to download NvChad starter"
+
+if [ -d "$HOME/.config/nvim" ]; then
+    mv "$HOME/.config/nvim" "$HOME/.config/nvim.backup"
+    check_status "Backed up existing ~/.config/nvim to ~/.config/nvim.backup" "Failed to backup existing ~/.config/nvim"
+fi
+
+git clone https://github.com/NvChad/starter ~/.config/nvim
+check_status "Downloaded NvChad starter" "Failed to download NvChad starter"
+
+if [ -d "$HOME/.config/nvim" ]; then
+    echo_green "NvChad starter downloaded successfully."
+else
+    echo_red "NvChad starter download failed."
+    exit 1
+fi
+
+echo_yellow "Installing Lua dependencies for NvChad"
+
+nvim --headless +PackerSync +qall
+check_status "Installed Lua dependencies for NvChad" "Failed to install Lua dependencies for NvChad"
+
+if nvim --headless +'lua require("lazy")' +qall; then
+    echo_green "NvChad and all dependencies are installed successfully."
+else
+    echo_red "Failed to install NvChad dependencies."
     exit 1
 fi
